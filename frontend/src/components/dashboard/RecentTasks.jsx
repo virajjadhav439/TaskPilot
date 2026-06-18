b32d538a69baacd2f026aedb7e955124afedd784
virajjadhav439 <virajjadhav439@gmail.com>
2026-06-19 00:22:56 +0530

    Initial Project Setup

---
+++ frontend/src/components/dashboard/RecentTasks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RecentTasks = () => {
+  return (
+    <div>RecentTasks</div>
+  )
+}
+
+export default RecentTasks
---
+++ frontend/src/components/dashboard/StatusCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StatusCards = () => {
+  return (
+    <div>StatusCards</div>
+  )
+}
+
+export default StatusCards
